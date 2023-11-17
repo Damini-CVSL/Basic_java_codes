e1cd3431b695bf241f675254bc83558b35da6af3
Damini-CVSL <126595128+Damini-CVSL@users.noreply.github.com>
2023-11-17 19:50:19 +0530

    Add files via upload

---
+++ Automorphic1.java
@@ -0,0 +1,10 @@
+class Automorphic1{
+    public static void main(String []a){
+        int x=5;
+        int y=x*x;
+        if(x%10 == y%10)
+            System.out.println("given number " +x+ " is Automorphic");
+        else
+            System.out.println("given number " +x+ " is Automorphic");
+    }
+}
