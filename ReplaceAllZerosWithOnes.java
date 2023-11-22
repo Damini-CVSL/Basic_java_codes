85eaaf4b01262652931206440fac5b2130dfaacb
Damini-CVSL <126595128+Damini-CVSL@users.noreply.github.com>
2023-11-22 11:03:41 +0530

    Add files via upload

---
+++ ReplaceAllZerosWithOnes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ReplaceAllZerosWithOnes{
+    public static void main(String[] a){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String str = Integer.toString(n);
+        int len = str.length();
+
+        String str1 = "";
+
+        for(int i=0; i<len; i++){
+            if(str.charAt(i) == '0')
+                str1 = str1 + '1';
+            else
+                str1 = str1 + str.charAt(i);
+        }
+        System.out.println("Converted number is : " +str1);
+        
+    }
+}
